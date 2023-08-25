55c640a1a2f0939c7b04c13a0db125484701f3bc
Okpabi Counsel <okpabicounsel@gmail.com>
2023-08-25 09:22:53 +0100

    edit sever.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.resolve(__dirname, "./client/dist")))
+app.use(express.static(path.resolve(__dirname, "./client/build")))
 
 app.use(express.json());
 
@@ -38,7 +38,7 @@ app.use(express.json());
 app.use("/api/auth/", authRouter);
 
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
   });
   
 
